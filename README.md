024fc7d73ff7948b26aef006ed40adad55d0215e
Filipe Lemos <filipe_lemos@live.com>
2022-09-19 17:56:30 -0300

    Primeiro commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Curso-de-Node.js
  Curso de Node.js no youtube canal Victor Lima - Guia do Programador
+
+Para a playlist das vídeo aulas [clique aqui](https://www.youtube.com/playlist?list=PLJ_KhUnlXUPtbtLwaxxUxHqvcNQndmI4B)
--- TrabalhandoComMongo/aprendendo.js
+++ TrabalhandoComMongo/aprendendo.js
@@ -3,12 +3,52 @@ const mongoose = require("mongoose");
 // Configuração Mongoose
 mongoose.Promise = global.Promise;
 mongoose
-  .connect("mongodb://localhost/aprendendo", {
-    useMongoClient: true,
-  })
-  .then(() => {
-    console.log("MongoDB Conectado!");
-  })
-  .catch((err) => {
-    console.log("Houve um erro ao se conectar ao mongoDB: " + err);
-  });
+	.connect("mongodb://localhost/aprendendo", {
+		useMongoClient: true,
+	})
+	.then(() => {
+		console.log("MongoDB Conectado!");
+	})
+	.catch((err) => {
+		console.log("Houve um erro ao se conectar ao mongoDB: " + err);
+	});
+// Model - Usuários
+// Definindo o Model
+const UsuarioSchema = mongoose.Schema({
+	nome: {
+		type: String,
+		require: true,
+	},
+	sobrenome: {
+		type: String,
+		require: true,
+	},
+	email: {
+		type: String,
+		require: true,
+	},
+	idade: {
+		type: Number,
+		require: true,
+	},
+	pais: {
+		type: String,
+	},
+});
+// Definindo a Collection
+mongoose.model("usuarios", UsuarioSchema);
+
+new UsuarioSchema({
+	nome: "Filipe",
+	sobrenome: "Lemos",
+	email: "email@email.com",
+	idade: 24,
+	pais: "Brasil",
+})
+	.save()
+	.then(() => {
+		console.log("Usuário criado com sucesso");
+	})
+	.catch((err) => {
+		console.log("Houve um erro ao registrar o usuário");
+	});
