83955df4a706fd961e4cf81966fd72fb05815268
Filipe Lemos <filipe_lemos@live.com>
2022-09-25 18:18:57 -0300

    Listando postagens

--- blogapp/routes/admin.js
+++ blogapp/routes/admin.js
@@ -45,7 +45,17 @@ router.get('/categorias/edit/:id', (req, res) => {
 })
 
 router.get('/postagens', (req, res) => {
-	res.render('admin/postagens')
+	Postagem.find()
+		.lean()
+		.populate('categoria')
+		.sort({ data: 'desc' })
+		.then((postagens) => {
+			res.render('admin/postagens', { postagens: postagens })
+		})
+		.catch((err) => {
+			req.flash('error_msg', 'Houve um erro ao listar as postagens')
+			res.redirect('/admin')
+		})
 })
 
 router.get('/postagens/add', (req, res) => {
